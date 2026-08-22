3615e86039339c9078ec56fae9006d2104b6026a
milankubat79-dev <milan.kubat79@gmail.com>
2026-08-22 13:32:39 +0200

    update

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// Service Worker — Hádej Vlajku! v7
-const CACHE = 'hadej-vlajku-v7';
+// Service Worker — Hádej Vlajku! v10
+const CACHE = 'hadej-vlajku-v10';
 const OFFLINE_URLS = [
   '/Hadej-vlajku/',
   '/Hadej-vlajku/index.html',
